3b87a04b55297d67647b40ca6b6eeb09b29406be
xZwei <31455754+xZwei@users.noreply.github.com>
2017-10-19 10:12:47 -0500

    Add files via upload
    +edit button modal
    +minor formatting changes to index.html

--- indexFB.js
+++ indexFB.js
@@ -2,16 +2,19 @@ $(document).ready(function(){
     var rootRef = firebase.database().ref().child("Instruments");
     var add_row = document.getElementById("add_row");
     
+    //unused
     function addbtnClick(){
         var pushData = { }
         
         rootRef.push().set();
     }
 	
-	function editBtnClick(){
-		
+    //unused
+	function editBtnClick(id){
+        
 	}
     
+    //on child_added event (see firebase doc), do things
     rootRef.on("child_added", snap =>{
         
         var type = snap.child("TYPE").val();
@@ -24,21 +27,87 @@ $(document).ready(function(){
 		//remove button
         $('#removebtn').off().on('click',function () {
 			var id = $(this).closest('tr').attr("id"); //retrieves the key from the id on tr element
-            $(this).closest('tr').remove(); //removes element from UI
-			rootRef.child(id).remove(); //removes from DB
-			
-			
+            $(this).closest('tr').remove();            //removes element from UI
+			rootRef.child(id).remove();                //removes entry from DB
         });
 		
 		//edit button
 		$('#editbtn').off().on('click',function () {
-			alert("EDIT!");
 			var id = $(this).closest('tr').attr("id"); //retrieves the key from the id on tr element
-			rootRef.child(id).update(); //removes from DB
-			
-			
+            modalEdit.open();                          //open modal
+			editBtnClick(id);			               //pass off id to editBtnCLick method for..?
         });
+        
+        //this code adds
         $('#table_body').append('<tr id="'+ key +'"><td class="nameq">' + type + '</td><td>' + brand + '</td><td>' + condition + '</td><td>' + mouthpiece + '</td><td>' + notes + '</td><td><button id="removebtn">Remove</button></td><td><button id="editbtn">Edit</button></td></tr>');
     })
+
+//TINGLE MODAL CODE FOR EDIT BUTTON
+    var modalEdit = new tingle.modal({
+        footer: true,
+        stickyFooter: false,
+        closeMethods: ['overlay', 'button', 'escape'],
+        closeLabel: "Close",
+        cssClass: ['custom-class-1', 'custom-class-2'],
+        onOpen: function() {
+            console.log('modal open');
+        },
+        onClose: function() {
+            console.log('modal closed');
+        },
+        beforeClose: function() {
+            return true; // close the modal
+        }
+    });
     
+    // set content
+    modalEdit.setContent('<h3>Add a new instrument</h3><form id="InstrumentForm"><div class="form-group"><label for="talkBRAND">Brand</label><input class="form-control" id="talkBRAND" placeholder="Brand of the instrument"></div><div class="form-group"><label for="talkCONDITION">Condition    </label><select id="talkCONDITION"><option value="" disabled selected>Select your option</option><option value="New">New</option><option value="Good">Good</option><option value="Fine">Fine</option><option value="Works">Works</option><option value="Poor">Poor</option><option value="Bad">Bad</option><option value="???">???</option></select></div><div class="form-group"><label for="talkHASMOUTHPIECE">Does the instrument have a mouthpiece?    </label><label><input type="radio" name="mouthPiece"  id="HASMOUTHPIECE_True" value="true" >Yes</label><label><input type="radio" name="mouthPiece" id="HASMOUTHPIECE_False" value="false" >No</label></div><div class="form-group"><label for="talkNOTES">Notes</label><input class="form-control" id="talkNOTES" placeholder="Notes on the current status of the instrument"></div><div class="form-group"><label for="talkTYPE">Type    </label><select id="talkTYPE"><option value="" disabled selected>Select your option</option><option value="Baritone">Baritone</option><option value="Tuba">Tuba</option><option value="Cornet">Cornet</option><option value="Euphonium">Euphonium</option><option value="Horn">Horn</option><option value="Mellophone">Mellophone</option><option value="Sousaphone">Sousaphone</option><option value="Trombone">Trombone</option><option value="Clarinet">Clarinet</option><option value="Flute">Flute</option><option value="Piccolo">Piccolo</option><option value="Saxophone">Saxophone</option><option value="Keyboard">Keyboard</option><option value="Percussion">Percussion</option></select></div>');
+    
+    /* TODO: 
+        -CHANGE THIS TO UPDATE EXISTING
+        -HAVE PREVIOUS VALUES LOADED IN
+    */
+    modalEdit.addFooterBtn('Submit', 'tingle-btn tingle-btn--primary', function() {
+        // Reference to the Instruments object in your Firebase database
+        var Instruments = firebase.database().ref("Instruments");
+        
+        // Get input values from each of the form elements
+        var BRAND = $("#talkBRAND").val();
+        var CONDITION = $("#talkCONDITION").val();
+        var HASMOUTHPIECE
+        if(document.getElementById('HASMOUTHPIECE_True').checked) {
+            HASMOUTHPIECE = $("#HASMOUTHPIECE_True").val();
+        }else if(document.getElementById('HASMOUTHPIECE_False').checked) {
+            HASMOUTHPIECE = $("#HASMOUTHPIECE_False").val();
+        } 
+        var NOTES = $("#talkNOTES").val();
+        var TYPE = $("#talkTYPE").val();
+
+        // Push a new Instrument to the database using those values
+        Instruments.push({
+            "BRAND": BRAND,
+            "CONDITION": CONDITION,
+            "HAS MOUTHPIECE": HASMOUTHPIECE,
+            "NOTES": NOTES,
+            "TYPE": TYPE
+        });
+        /* Get the single most recent Instrument from the database and
+           update the table with its values. This is called every time the child_added
+           event is triggered on the Instruments Firebase reference, which means
+           that this will update EVEN IF you don't refresh the page. Magic.             */
+        Instruments.limitToLast(1).on('child_added', function(childSnapshot) {
+          // Get the Instrument data from the most recent snapshot of data
+          // added to the Instruments list in Firebase
+          Instrument = childSnapshot.val();
+          // Update the HTML to display the Instrument text
+          $("#BRAND").html(Instrument.BRAND)
+          $("#CONDITION").html(Instrument.CONDITION)
+          $("#HASMOUTHPIECE").html(Instrument.HASMOUTHPIECE)
+          $("#NOTES").html(Instrument.NOTES)
+          $("#TYPE").html(Instrument.TYPE)
+        });
+					
+        modalEdit.close();
+    });
+//END MODAL CODE
 });
