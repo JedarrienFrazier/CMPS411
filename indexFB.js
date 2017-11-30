7bcf6204e21e73cc925c3420d05e5b794f696645
josephschenck <joseph.schenck@selu.edu>
2017-11-30 13:22:05 -0600

    Added files for final presentation
    Added authentication, SLU tag, Make tag, and all other changes made front between the midterms to now.

--- indexFB.js
+++ indexFB.js
@@ -7,36 +7,48 @@ $(document).ready(function(){
         globalID = id;
 	}
     
-    function onAddClick(){
-        alert("success!");
-        modalAdd.open();
-    }
-    
     //on child_added event (see firebase doc), do things
     rootRef.on("child_added", snap =>{
         
         var type = snap.child("TYPE").val();
+        var serial = snap.child("SERIAL").val();
         var brand = snap.child("BRAND").val();
         var condition = snap.child("CONDITION").val();
         var mouthpiece = snap.child("HAS MOUTHPIECE").val();
         var notes = snap.child("NOTES").val();
+		var slu = snap.child("SLU").val();
+		var make = snap.child("MAKE").val();
+        var status = snap.child("STATUS").val();
 		var key = snap.key;
-		//remove button
-        $('button').off().on('click',function () {
-			var id = $(this).closest('tr').attr("id"); //retrieves the key from the id on tr element
-            $(this).closest('tr').remove();            //removes element from UI
-			rootRef.child(id).remove();                //removes entry from DB
-        });
-		
-		//edit button
-		$('p').off().on('click',function () {
-			var id = $(this).closest('tr').attr("id"); //retrieves the key from the id on tr element
-            modalEdit.open();                          //open modal
-			setID(id);			                       //pass off id to editBtnCLick method for..?
-        });
+        var test = status;
         
-        //this code adds
-        $('#table_body').append('<tr id="'+ key +'"><td class="nameq">' + type + '</td><td>' + brand + '</td><td>' + condition + '</td><td>' + mouthpiece + '</td><td>' + notes + '</td><td><button id="button">Remove</button></td><td><p>EDIT</p></td></tr>');
+        if(status == ""){
+            status = "Available";
+        }
+        
+        //code for buttons
+        $("h3").off().on('click',function() {
+            if($("#addButton:hover").length > 0){
+                modalAdd.open();
+            }
+        });
+        $("tr").off().on('click',function() {
+            var id = $(this).closest('tr').attr("id");
+            rmid = "#rmbtn" + id;
+            edid = "#edbtn" + id;
+            //code for remove
+            if($(rmid + ":hover").length > 0){
+                $(this).closest('tr').remove();            //removes element from UI
+			    rootRef.child(id).remove();                //removes entry from DB
+            }
+            //code for edit
+            else if($(edid + ":hover").length > 0){
+                modalEdit.open();                          //open modal
+			    setID(id);			                       //pass off id to editBtnCLick method for..?
+            }
+        });
+        //this code adds the data to the table
+        $('#table_body').append('<tr id="'+ key +'"><td>' + serial + '</td><td>' + type + '</td><td>' + brand + '</td><td>' + condition + '</td><td>' + mouthpiece + '</td><td>' + notes + '</td><td>' + status + '</td><td><center><button class="btn btn-danger btn-sm" id="rmbtn' + key +'">Remove</button></center></td><td><center><button class="btn btn-info btn-sm" id = "edbtn' + key +'">Edit</button></center></td></tr>');
     })
 
 //TINGLE MODAL CODE FOR EDIT BUTTON
@@ -47,66 +59,145 @@ $(document).ready(function(){
         closeLabel: "Close",
         cssClass: ['custom-class-1', 'custom-class-2'],
         onOpen: function() {
-            console.log('modal open');
+            console.log('edit modal open');
+            setEditModalContent();
         },
         onClose: function() {
             window.location.reload();
-            console.log('modal closed');
+            console.log('edit modal closed');
         },
         beforeClose: function() {
             return true; // close the modal
         }
     });
     
-    // set content
-    modalEdit.setContent('<h3>Edit Details  </h3><form id="InstrumentForm"><div class="form-group"><label for="talkBRAND">Brand</label><input class="form-control" id="talkBRAND" placeholder="Brand of the instrument"></div><div class="form-group"><label for="talkCONDITION">Condition    </label><br><select id="talkCONDITION"><option value="" disabled selected>Select your option</option><option value="New">New</option><option value="Good">Good</option><option value="Fine">Fine</option><option value="Works">Works</option><option value="Poor">Poor</option><option value="Bad">Bad</option><option value="???">???</option></select></div><div class="form-group"><label for="talkHASMOUTHPIECE">Does the instrument have a mouthpiece?    </label><br><label><input type="radio" name="mouthPiece"  id="HASMOUTHPIECE_True" value="true" >Yes</label>&nbsp<label><input type="radio" name="mouthPiece" id="HASMOUTHPIECE_False" value="false" >No</label></div><div class="form-group"><label for="talkNOTES">Notes</label><input class="form-control" id="talkNOTES" placeholder="Notes on the current status of the instrument"></div><div class="form-group"><label for="talkTYPE">Type</label><input class="form-control" id="talkTYPE" placeholder="Type of instrument"></div>');
+    function setEditModalContent(){
+        var id = globalID;
+        var childRef = firebase.database().ref().child("Instruments").child(id);
+        //vars for loading into edit
+        childRef.on("value", snap => {
+            var type = snap.child("TYPE").val();
+            var serial = snap.child("SERIAL").val();
+            var brand = snap.child("BRAND").val();
+            var condition = snap.child("CONDITION").val();
+            var mouthpiece = snap.child("HAS MOUTHPIECE").val();
+            var notes = snap.child("NOTES").val();
+            var slu = snap.child("SLU").val();
+			var make = snap.child("MAKE").val();
+            var status = snap.child("STATUS").val();
+            
+            modalEdit.setContent('<h3>Edit Details  </h3><form id="InstrumentForm"><div class="form-group"><div class="form-group"><label>Serial #</label><input class="form-control" id="talkSERIAL" value="'+ serial +'"></div><div class="form-group"><label>Type</label><input class="form-control" id="talkTYPE" value="'+ type +'"></div><label>Brand</label><input class="form-control" id="talkBRAND" value="'+ brand +'"></div><div class="form-group"><label>Condition</label><br><select id="talkCONDITION"><option value="'+ condition +'"></option><option value="New">New</option><option value="Good">Good</option><option value="Fine">Fine</option><option value="Works">Works</option><option value="Poor">Poor</option><option value="Bad">Bad</option><option value="???">???</option></select></div><div class="form-group"><label>Does the instrument have a mouthpiece?</label><br><select id="talkHASMOUTHPIECE"><option value="'+ mouthpiece +'"></option><option value="true">Yes</option><option value="false">No</option><option value="null">NA</option></select></div><div class="form-group"><label>Notes</label><input class="form-control" id="talkNOTES" value="'+ notes +'"><div class="form-group"><label>SLU</label><input class="form-control" id="talkSLU" value="'+ slu +'"><div class="form-group"><label>Make</label><input class="form-control" id="talkMAKE" value="'+ make +'"><div class="form-group"></div><div class="form-group"><label>Checked Out</label><input class="form-control" id="talkSTATUS" value="'+ status +'"> </div>');
+          });
+    }
     
-    /* TODO: 
-        -CHANGE THIS TO UPDATE EXISTING
-        -HAVE PREVIOUS VALUES LOADED IN
-    */
     modalEdit.addFooterBtn('Submit', 'tingle-btn tingle-btn--primary', function() {
         // Reference to the Instruments object in your Firebase database
         var Instruments = firebase.database().ref("Instruments");
         
         // Get input values from each of the form elements
         var BRAND = $("#talkBRAND").val();
+        var SERIAL = $("#talkSERIAL").val();
         var CONDITION = $("#talkCONDITION").val();
-        var HASMOUTHPIECE
-        if(document.getElementById('HASMOUTHPIECE_True').checked) {
-            HASMOUTHPIECE = $("#HASMOUTHPIECE_True").val();
-        }else if(document.getElementById('HASMOUTHPIECE_False').checked) {
-            HASMOUTHPIECE = $("#HASMOUTHPIECE_False").val();
-        } 
+        var HASMOUTHPIECE = $("#talkHASMOUTHPIECE").val();
         var NOTES = $("#talkNOTES").val();
+        var SLU = $("#talkSLU").val();
+        var MAKE = $("#talkMAKE").val();
         var TYPE = $("#talkTYPE").val();
-        
-        
+        var STATUS = $("#talkSTATUS").val();
+
         var updates = {};
-        updates['Instruments/' + globalID + '/BRAND'] = BRAND;
-        updates['Instruments/' + globalID + '/CONDITION'] = CONDITION;
-        updates['Instruments/' + globalID + '/HAS MOUTHPIECE'] = HASMOUTHPIECE;
-        updates['Instruments/' + globalID + '/NOTES'] = NOTES;
-        updates['Instruments/' + globalID + '/TYPE'] = TYPE;
-        firebase.database().ref().update(updates);
-        
-        /* Get the single most recent Instrument from the database and
-           update the table with its values. This is called every time the child_added
-           event is triggered on the Instruments Firebase reference, which means
-           that this will update EVEN IF you don't refresh the page. Magic.   */          
+        updates[globalID + '/BRAND'] = BRAND;
+        updates[globalID + '/SERIAL'] = SERIAL;
+        updates[globalID + '/CONDITION'] = CONDITION;
+        updates[globalID + '/HAS MOUTHPIECE'] = HASMOUTHPIECE;
+        updates[globalID + '/NOTES'] = NOTES;
+        updates[globalID + '/SLU'] = SLU;
+        updates[globalID + '/MAKE'] = MAKE;
+        updates[globalID + '/TYPE'] = TYPE;
+        updates[globalID + '/STATUS'] = STATUS;
+        Instruments.update(updates);
+       
+        //Figure out how to make this update the html to match the updated database...
+        Instruments.on("child_changed", function(snapshot) {
+            alert("child changed!");
+            // Get the Instrument data from the most recent snapshot of data
+            // added to the Instruments list in Firebase
+            Instrument = snapshot.val();
+            // Update the HTML to display the Instrument text
+            $("#BRAND").html(Instrument.BRAND)
+            $("#SERIAL").html(Instrument.SERIAL)
+            $("#CONDITION").html(Instrument.CONDITION)
+            $("#HASMOUTHPIECE").html(Instrument.HASMOUTHPIECE)
+            $("#NOTES").html(Instrument.NOTES)
+            $("#SLU").html(Instrument.SLU)
+            $("#MAKE").html(Instrument.MAKE)
+            $("#TYPE").html(Instrument.TYPE)
+            $("#STATUS").html(Instrument.STATUS)
+        });
+					
+        modalEdit.close();
+    });
+//END MODAL EDIT CODE
+//MODAL CODE FOR ADD BUTTON
+    var modalAdd = new tingle.modal({
+        footer: true,
+        stickyFooter: false,
+        closeMethods: ['overlay', 'button', 'escape'],
+        closeLabel: "Close",
+        cssClass: ['custom-class-1', 'custom-class-2'],
+        onOpen: function() {
+            console.log('add modal open');
+            setAddModalContent();
+        },
+        onClose: function() {
+            console.log('add modal closed');
+        },
+        beforeClose: function() {
+            return true; // close the modal
+        }
+    });
+    function setAddModalContent(){
+        modalAdd.setContent('<h3>Add Instrument</h3><form id="InstrumentForm"><div class="form-group"><div class="form-group"><label>Serial #</label><input class="form-control" id="talkSERIAL" placeholder="Serial #"></div><div class="form-group"><label>Type</label><input class="form-control" id="talkTYPE" placeholder="Instrument Type"></div><label>Brand</label><input class="form-control" id="talkBRAND" placeholder="Brand"></div><div class="form-group"><label>Condition</label><br><select id="talkCONDITION"><option></option><option value="New">New</option><option value="Good">Good</option><option value="Fine">Fine</option><option value="Works">Works</option><option value="Poor">Poor</option><option value="Bad">Bad</option><option value="???">???</option></select></div><div class="form-group"><label>Does the instrument have a mouthpiece?</label><br><select id="talkHASMOUTHPIECE"><option></option><option value="true">Yes</option><option value="false">No</option><option value="null">NA</option></select></div><div class="form-group"><label>Notes</label><input class="form-control" id="talkNOTES" placeholder="Notes"></div><div class="form-group"><label>SLU</label><input class="form-control" id="talkSLU" placeholder="SLU"><div class="form-group"><label>Make</label><input class="form-control" id="talkMAKE" placeholder="The make of the instrument"></div><div class="form-group"><label>Checked Out</label><input class="form-control" id="talkSTATUS" placeholder="In/Out"></div>');
+    }
+    modalAdd.addFooterBtn('Submit', 'tingle-btn tingle-btn--primary', function() {
+        var Instruments = firebase.database().ref("Instruments");
+        var BRAND = $("#talkBRAND").val();
+        var SERIAL = $("#talkSERIAL").val();
+        var CONDITION = $("#talkCONDITION").val();
+        var HASMOUTHPIECE = $("#talkHASMOUTHPIECE").val();
+        var NOTES = $("#talkNOTES").val();
+        var SLU = $("#talkSLU").val();
+        var MAKE = $("#talkMAKE").val();
+        var TYPE = $("#talkTYPE").val();
+        var STATUS = $("#talkSTATUS").val();
+        Instruments.push({
+            "BRAND": BRAND,
+            "CONDITION": CONDITION,
+            "HAS MOUTHPIECE": HASMOUTHPIECE,
+            "NOTES": NOTES,
+            "SLU": SLU,
+            "MAKE": MAKE,
+            "TYPE": TYPE,
+            "SERIAL": SERIAL,
+            "STATUS": STATUS
+        });
         Instruments.limitToLast(1).on('child_added', function(childSnapshot) {
             // Get the Instrument data from the most recent snapshot of data
             // added to the Instruments list in Firebase
             Instrument = childSnapshot.val();
             // Update the HTML to display the Instrument text
             $("#BRAND").html(Instrument.BRAND)
+            $("#SERIAL").html(Instrument.SERIAL)
             $("#CONDITION").html(Instrument.CONDITION)
             $("#HASMOUTHPIECE").html(Instrument.HASMOUTHPIECE)
             $("#NOTES").html(Instrument.NOTES)
+            $("#SLU").html(Instrument.SLU)
+            $("#MAKE").html(Instrument.MAKE)
             $("#TYPE").html(Instrument.TYPE)
+            $("#STATUS").html(Instrument.STATUS)
         });
 					
-        modalEdit.close();
+        modalAdd.close();
     });
-//END MODAL EDIT CODE
+//END MODAL ADD CODE
 });
